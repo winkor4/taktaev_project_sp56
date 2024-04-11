d207f34f653cc37575c24b9ce6af972721b1d3ce
ivan taktaev <winkor4@gmail.com>
2024-04-11 14:52:12 +0300

    config

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/winkor4/taktaev_project_sp56
+
+go 1.21.5
+
+require github.com/caarlos0/env/v6 v6.10.1
---
+++ internal/pkg/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"flag"
+
+	"github.com/caarlos0/env/v6"
+)
+
+type Config struct {
+	RunAddress           string `env:"RUN_ADDRESS"`
+	DatabaseURI          string `env:"DATABASE_URI"`
+	AccuralSystemAddress string `env:"ACCURAL_SYSTEM_ADDRESS"`
+}
+
+var (
+	flagRunAddress           string
+	flagDatabaseURI          string
+	flagAccuralSystemAddress string
+)
+
+func Parse() (*Config, error) {
+
+	cfg := new(Config)
+
+	flag.StringVar(&flagRunAddress, "a", "", "srv run addres and port")
+	flag.StringVar(&flagDatabaseURI, "d", "", "PostgresSQL server")
+	flag.StringVar(&flagAccuralSystemAddress, "r", "", "accural system address and port")
+
+	err := env.Parse(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if cfg.RunAddress == "" {
+		cfg.RunAddress = flagRunAddress
+	}
+	if cfg.DatabaseURI == "" {
+		cfg.DatabaseURI = flagDatabaseURI
+	}
+	if cfg.AccuralSystemAddress == "" {
+		cfg.AccuralSystemAddress = flagAccuralSystemAddress
+	}
+
+	return cfg, nil
+
+}
